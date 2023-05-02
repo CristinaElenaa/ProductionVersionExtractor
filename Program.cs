ff0b3979caecbfce0d5b1bb96c53594ebf2c2dd1
Elena Cristina Sisu <cris_elena25@yahoo.com>
2023-05-02 12:06:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProductionVersionExtractor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var versionExtractor = new VersionExtractor();
+
+        List<string> versions = new List<string> { "2.5.0-dev.1", "2.4.2-5354", "2.4.2-test.675", "2.4.1-integration.1" };
+
+        string productionVersion = versionExtractor.GetProductionVersion(versions);
+        Console.WriteLine(productionVersion);
+    }
+}
---
+++ VersionExtractor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionVersionExtractor
+{
+    public class VersionExtractor
+    {
+        public string GetProductionVersion(List<string> versions)
+        {
+            Version productionVersion = null;
+            foreach (string version in versions)
+            {
+                string[] versionParts = version.Split('-');
+                string[] versionNumbers = versionParts[0].Split('.');
+                if (versionNumbers.Length < 3)
+                {
+                    continue;
+                }
+
+                Version currentVersion = new Version(
+                    int.Parse(versionNumbers[0]),
+                    int.Parse(versionNumbers[1]),
+                    int.Parse(versionNumbers[2]));
+
+                if (productionVersion == null || currentVersion > productionVersion)
+                {
+                    productionVersion = currentVersion;
+                }
+            }
+
+            return productionVersion?.ToString();
+        }
+
+
+    }
+}
